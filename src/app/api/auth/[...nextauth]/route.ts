09b4821008dbf923c9452450905777ba685476df
Cody Lambert <codylambertdev@gmail.com>
2024-11-02 14:41:48 -0700

    pull out google auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }: { token: any, account: any }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any, token: any }) {
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-};
+import { authOptions } from "@/lib/google";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ src/lib/google.ts
@@ -0,0 +1,22 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }: { token: any; account: any }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
